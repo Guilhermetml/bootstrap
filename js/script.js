36e0962bf63ac87146dc9087ec71840a013bdb79
Guilherme <guilhermeleite@byronsolutions.com>
2022-02-11 19:08:57 -0300

    site pronto sem responsivo

--- js/script.js
+++ js/script.js
@@ -114,7 +114,47 @@ $(document).ready(function() {
   setTimeout(function() {
     
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
 
   }, 250);
+
+  //Filtro do portfólio
+
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    // Não ta trocando de categoria no site
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes)
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+
+  }
 
 });

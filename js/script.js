1998f8a8766813ca4550029178b40f0de4fdf222
Guilherme <guilhermeleite@byronsolutions.com>
2022-02-11 19:56:31 -0300

    site responsivo com leves erros

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-$(document).ready(function() {
+$( document ).ready(function() {
 
   // Progress bar
   let containerA = document.getElementById("circleA");
 
   let circleA = new ProgressBar.Circle(containerA,{
 
-    color:'#64DAF9',
+    color: '#64DAF9',
     strokeWidth: 8,
     duration: 1400,
     from: { color: '#AAA' },
-    to: { color:'#65DAF9'},
+    to: { color: '#65DAF9'},
 
     step: function(state, circle) {
-
       circle.path.setAttribute('stroke', state.color);
 
       let value = Math.round(circle.value() * 60);
-
       circle.setText(value);
 
     }
@@ -27,18 +25,16 @@ $(document).ready(function() {
 
   let circleB = new ProgressBar.Circle(containerB,{
 
-    color:'#64DAF9',
+    color: '#64DAF9',
     strokeWidth: 8,
     duration: 1600,
     from: { color: '#AAA' },
-    to: { color:'#65DAF9'},
+    to: { color: '#65DAF9'},
 
     step: function(state, circle) {
-
       circle.path.setAttribute('stroke', state.color);
 
       let value = Math.round(circle.value() * 254);
-
       circle.setText(value);
 
     }
@@ -49,18 +45,16 @@ $(document).ready(function() {
 
   let circleC = new ProgressBar.Circle(containerC,{
 
-    color:'#64DAF9',
+    color: '#64DAF9',
     strokeWidth: 8,
     duration: 2000,
     from: { color: '#AAA' },
-    to: { color:'#65DAF9'},
+    to: { color: '#65DAF9'},
 
     step: function(state, circle) {
-
       circle.path.setAttribute('stroke', state.color);
 
       let value = Math.round(circle.value() * 32);
-
       circle.setText(value);
 
     }
@@ -69,7 +63,7 @@ $(document).ready(function() {
 
   let containerD = document.getElementById("circleD");
 
-  let circleD = new ProgressBar.Circle(containerD,{
+  let circleD = new ProgressBar.Circle(containerD, {
 
     color:'#64DAF9',
     strokeWidth: 8,
@@ -78,45 +72,42 @@ $(document).ready(function() {
     to: { color:'#65DAF9'},
 
     step: function(state, circle) {
-
       circle.path.setAttribute('stroke', state.color);
 
       let value = Math.round(circle.value() * 5243);
-
       circle.setText(value);
 
     }
 
   });
 
-  // iniciando o loader quando o usuário chega no elemento
+  // iniciando loaders quando a usuário chegar no elemento
   let dataAreaOffset = $('#data-area').offset();
+  // stop serve para a animação não carregar mais que uma vez
   let stop = 0;
 
-  $(window).scroll(function(e) {
+  $(window).scroll(function (e) {
 
     let scroll = $(window).scrollTop();
 
     if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
-
       circleA.animate(1.0);
       circleB.animate(1.0);
       circleC.animate(1.0);
       circleD.animate(1.0);
 
       stop = 1;
-
     }
 
   })
 
   // Parallax
+
+  // setTimeout serve para carregar primeiro as imagens
   setTimeout(function() {
-    
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
     $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
-
-  }, 250);
+  }, 200);
 
   //Filtro do portfólio
 
@@ -128,7 +119,6 @@ $(document).ready(function() {
     $('.main-btn').removeClass('active');
     $(this).addClass('active');
 
-    // Não ta trocando de categoria no site
     if(type == 'dsg-btn') {
       eachBoxes('dsg', boxes)
     } else if(type == 'dev-btn') {
@@ -154,7 +144,42 @@ $(document).ready(function() {
         }
       });
     }
-
   }
+
+  // scroll para as seções
+
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let teamSection = $('#team-area');
+  let portfolioSection = $('#portfolio-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'team-menu') {
+      scrollTo = teamSection;
+    } else if(btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+  });
 
 });
